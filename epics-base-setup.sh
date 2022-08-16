5b5d49360b214aca366e6dead7bb59a0e581fa9d
fkraemer <fkraemer@gemini.edu>
2022-08-15 23:51:02 -1000

    version4 (instead of version 3) patches applied

--- epics-base-setup.sh
+++ epics-base-setup.sh
@@ -7,8 +7,8 @@ cp configure/CONFIG_SITE.local tmp/configure/
 pushd tmp
 
 patch -p1 < ../0001-rtems-Close-NTP-socket.patch
-patch -p1 < ../v3-0001-rtems-Provide-an-NTP-version-of-osdTime-for-POSIX.patch
-patch -p1 < ../v3-0002-rtems-Check-NTP-env-variable-each-NTP-get-if-set-.patch
+patch -p1 < ../v4-0001-rtems-Provide-an-NTP-version-of-osdTime-for-POSIX.patch
+patch -p1 < ../v4-0002-rtems-Check-NTP-env-variable-each-NTP-get-if-set-.patch
 make distclean uninstall all
 
 popd
