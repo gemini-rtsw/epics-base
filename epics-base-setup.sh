1b6d2f4adcf3d5d65b31f3d132bcc88dbcbfb186
fkraemer <fkraemer@gemini.edu>
2022-08-24 08:23:34 -1000

    init and fetch submodules

--- epics-base-setup.sh
+++ epics-base-setup.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
 set -x
+
+# fetch submodules
+git submodule init
+git submodule update
+
+# create a temporary copy of epics-base submodule to apply config and patches
 cp -r vendor/epics-base tmp
 cp configure/CONFIG_SITE.local tmp/configure/
 
 pushd tmp
 
+# apply patches
 patch -p1 < ../0001-rtems-Close-NTP-socket.patch
 patch -p1 < ../v4-0001-rtems-Provide-an-NTP-version-of-osdTime-for-POSIX.patch
 patch -p1 < ../v4-0002-rtems-Check-NTP-env-variable-each-NTP-get-if-set-.patch
